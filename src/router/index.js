9278919c21784ca87abceb10426ed94184d89f6a
Alexander Strikalov <aleksandr.strikalov@noveogroup.com>
2024-09-29 11:33:54 +0200

    finish project
    finish project

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,76 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+import GlobalFeed from '@/views/GlobalFeed.vue'
+import YourFeed from '@/views/YourFeed.vue'
+import TagFeed from '@/views/TagFeed.vue'
+import Register from '@/views/Register.vue'
+import Login from '@/views/Login.vue'
+import Article from '@/views/Article.vue'
+import CreateArticle from '@/views/CreateArticle.vue'
+import EditArticle from '@/views/EditArticle.vue'
+import Settings from '@/views/Settings.vue'
+import UserProfile from '@/views/UserProfile'
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/register',
+    name: 'register',
+    component: Register
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/',
+    name: 'globalFeed',
+    component: GlobalFeed
+  },
+  {
+    path: '/feed',
+    name: 'yourFeed',
+    component: YourFeed
+  },
+  {
+    path: '/tags/:slug',
+    name: 'tag',
+    component: TagFeed
+  },
+  {
+    path: '/articles/new',
+    name: 'createArticle',
+    component: CreateArticle
+  },
+  {
+    path: '/articles/:slug',
+    name: 'article',
+    component: Article
+  },
+  {
+    path: '/articles/:slug/edit',
+    name: 'editArticle',
+    component: EditArticle
+  },
+  {
+    path: '/settings',
+    name: 'settings',
+    component: Settings
+  },
+  {
+    path: '/profiles/:slug',
+    name: 'userProfile',
+    component: UserProfile
+  },
+  {
+    path: '/profiles/:slug/favorites',
+    name: 'userProfileFavorites',
+    component: UserProfile
   }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
